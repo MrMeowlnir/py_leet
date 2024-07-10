ac7469090b77a1bfb70d68fe93bcc74d5ef0d17b
mrmeowlnir <dmsdens@gmail.com>
2024-07-10 21:19:28 +0300

    2023 with tests

--- src/medium/2023. Number of Pairs of Strings With Concatenation Equal to Target.py
+++ src/medium/2023. Number of Pairs of Strings With Concatenation Equal to Target.py
@@ -1,3 +1,7 @@
+from src.tests import *
+from src.bcolor import bcolors
+
+
 class Solution:
     def numOfPairs(self, nums: list[str], target: str) -> int:
         count = 0
@@ -10,12 +14,34 @@ class Solution:
 
 if __name__ == '__main__':
     solution = Solution()
-    cases = [dict({'nums': ["777", "7", "77", "77"],
-                   'target': "7777"}),
-             dict({'nums': ["123", "4", "12", "34"],
-                   'target': "1234"}),
-             dict({'nums': ["1", "1", "1"],
-                   'target': "11"}),
-             ]
+    func_test = solution.numOfPairs
+    print(type(func_test))
+    cases = [
+        {'data':
+            {
+                'nums': ["777", "7", "77", "77"],
+                'target': "7777"
+            },
+            'result': 1},
+        {'data':
+            {
+                'nums': ["123", "4", "12", "34"],
+                'target': "1234"
+            },
+            'result': 2},
+        {'data':
+            {
+                'nums': ["1", "1", "1"],
+                'target': "11",
+            },
+            'result': 6}
+    ]
+    case_count = 0
+    failed = []
     for case in cases:
-        print(solution.numOfPairs(**case))
+        case_count += 1
+        test_prints(func_test, case['data'], case['result'], case_count, failed)
+    if failed:
+        print(f'{bcolors.WARNING}Total failed {len(failed)} cases{bcolors.ENDC}')
+    for fail in failed:
+        print(f'{fail}')
