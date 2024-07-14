def21ae9eb7cb3430b0604809a4b16fe011996b3
mrmeowlnir <dmsdens@gmail.com>
2024-07-14 20:15:27 +0300

    726

--- src/hard/726. Number of Atoms.py
+++ src/hard/726. Number of Atoms.py
@@ -21,7 +21,8 @@ class Solution:
 
         For example, "(H2O2)" and "(H2O2)3" are formulas.
         Return the count of all elements as a string in the following form: the first name (in sorted order),
-        followed by its count (if that count is more than 1), followed by the second name (in sorted order), followed by its count (if that count is more than 1), and so on.
+        followed by its count (if that count is more than 1), followed by the second name (in sorted order), followed
+        by its count (if that count is more than 1), and so on.
 
         The test cases are generated so that all the values in the output fit in a 32-bit integer.
         """
