59c87e58dac0a8618acdb4da48685ab44882c39b
mrmeowlnir <dmsdens@gmail.com>
2024-07-18 19:18:16 +0300

    1915 faster solvation

--- src/medium/1915. Number of Wonderful Substrings.py
+++ src/medium/1915. Number of Wonderful Substrings.py
@@ -14,26 +14,17 @@ class Solution:
 
         A substring is a contiguous sequence of characters in a string.
         """
-        counts = [0] * 1024
-        counts[0] = 1
-        wonderfulCount = 0
+        count = [0] * 1024
         mask = 0
-
+        count[0] = 1
+        result = 0
         for c in word:
             mask ^= 1 << (ord(c) - ord('a'))
-            counts[mask] += 1
-
-        for msk in range(1024):
-            if counts[msk] != 0:
-                cntAtMsk = counts[msk]
-                wonderfulCount += ((cntAtMsk - 1) * cntAtMsk) >> 1
-                bit = 1
-                while bit <= msk:
-                    if (msk & bit) != 0:
-                        wonderfulCount += counts[msk ^ bit]
-                    bit <<= 1
-
-        return wonderfulCount
+            result += count[mask]
+            for i in range(10):
+                result += count[mask ^ (1 << i)]
+            count[mask] += 1
+        return result
 
 
 if __name__ == '__main__':
